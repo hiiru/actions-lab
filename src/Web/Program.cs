d8f80ab9f698830006d41c327523a5a914b08b75
Hiiru <hiiru@unknown>
2024-09-03 14:01:03 +0900

    update

--- src/Web/Program.cs
+++ src/Web/Program.cs
@@ -20,7 +20,7 @@ app.UseHttpsRedirection();
 
 var summaries = new[]
 {
-    "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching", "dsfasd"
+    "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching", "dsfasd3"
 };
 
 app.MapGet("/weatherforecast", () =>
